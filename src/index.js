79b041986ae9e9f4dbeaf288107dd2ecd63f6894
Aleksandra <alex.sarychewa2014@yandex.ru>
2023-05-01 16:52:52 +0300

    refactor: change css-classes naming and way to display key text

--- src/index.js
+++ src/index.js
@@ -1,90 +1,60 @@
-import './index.html'
-import './index.scss'
-
+import './index.html';
+import './index.scss';
 
 const keyboardLettersEn = [
-  ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
-  ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del'],
-  ['Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
-  ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'Shift'],
-  ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '◄', '▼', '►', 'Ctrl'],
+  '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
+  'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del',
+  'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter',
+  'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'Shift',
+  'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '◄', '▼', '►', 'Ctrl',
 ];
-const specialLetters = ['Backspace','Tab', 'Del', 'Caps Lock', 'Enter', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', '◄', '▼', '►', '▲']
-function findSpecialClass(key, index){
-  let flag = false;
-  let className = null;
-  if (key === 'Enter'){
-    flag = true;
-    className = 'enter';
-  }
-  else if (key === 'Shift'){
-    if (index === 0){
-      flag = true;
-      className = 'shiftLft';
-    }
-    else{
-      flag = true;
-      className = 'shiftRgt';
-    }
-  }
-  else if (key === 'Caps Lock'){
-    flag = true;
-    className = 'caps';
-  }
-  else if (key === 'Backspace'){
-    flag = true;
-    className = 'backspace';
-  }
-  else if (key === 'Space'){
-    flag = true;
-    className = 'space';
-  }
-  else if (key === 'Tab'){
-    flag = true;
-    className = 'tab';
-  }
-  return {
-    flag: flag,
-    class: className,
-  }
-}
-function createKeys(row){
-  const keys = keyboardLettersEn[row];
+
+const keyboardCodes = [
+  'Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
+  'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete',
+  'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter',
+  'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight',
+  'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight',
+];
+const specialLetters = ['Backspace', 'Tab', 'Del', 'Caps Lock', 'Enter', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', '◄', '▼', '►', '▲'];
+const rowLength = [14, 15, 13, 13, 9];
+const keyNumberStartRow = [0, 14, 29, 42, 55];
+
+function createKeys(row) {
+  const keys = keyboardLettersEn
+    .slice(keyNumberStartRow[row], keyNumberStartRow[row] + rowLength[row]);
   const keyRow = [];
-  keys.forEach((key, index)=>{
+  keys.forEach((key, index) => {
     const keyElement = document.createElement('button');
     keyElement.classList.add('keyboard__key');
-    keyElement.textContent=key;
-    if (specialLetters.includes(key)){
+    keyElement.textContent = key;
+    if (specialLetters.includes(key)) {
       keyElement.classList.add('keyboard__key_special');
-    }
-    else{
+    } else {
       keyElement.classList.add('keyboard__key_classic');
     }
+    keyElement.classList.add(keyboardCodes[keyNumberStartRow[row] + index]);
     keyRow.push(keyElement);
-    let isSpecialClass = findSpecialClass(key, index);
-    if (isSpecialClass.flag){
-      keyElement.classList.add(isSpecialClass.class);
-    }
-  })
+  });
   return keyRow;
 }
-function initKeyboardLayout(){
+
+function initKeyboardLayout() {
   const keyboardRows = [];
-  for (let i = 0; i < 5; i++){
+  for (let i = 0; i < 5; i += 1) {
     const row = document.createElement('div');
     row.classList.add('keyboard__row');
     const keys = createKeys(i);
     row.append(...keys);
     keyboardRows.push(row);
   }
-  return keyboardRows
+  return keyboardRows;
 }
 function initPage() {
   const body = document.querySelector('body');
   body.classList.add('page');
   const wrapper = document.createElement('div');
-  wrapper.classList.add("wrapper");
+  wrapper.classList.add('wrapper');
   const title = document.createElement('h1');
   title.classList.add('title');
   title.textContent = 'Virtual Keyboard';
